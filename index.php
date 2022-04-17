86f698724971f6bae84b6bf5d7a68e3be417e994
ZohirB <zohirbochi.zb@gmail.com>
2022-04-17 21:01:30 +0300

    main

--- index.php
+++ index.php
@@ -5,50 +5,8 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<h2>Victim Table</h2>
-<div class="table-wrapper">
-    <table class="fl-table">
-        <thead>
-			<tr>
-				<th>ID Victim</th>
-				<th>ID GS</th>
-				<th>Age</th>
-				<th>Blood Type</th>
-				<th>ID Territory</th>
-				<th>Date Of Murder</th>
-			</tr>
-        </thead>
-
-<?php
-$conn = mysqli_connect("localhost", "root", "", "se_project");
-// Check connection, if failed show error
-if ($conn-> connect_error) {
-    die("Connection failed:". $conn-> connect_error);
-}
-$sql = "SELECT * from victim";
-$result = $conn -> query($sql);
-
-if ($result -> num_rows > 0){
-// Output data for each row
-    while ($row = $result -> fetch_assoc()){
-        echo "<tr>
-		<td>". $row["ID_Victim"]. "</td>
-		<td>". $row["ID_GS"]. "</td>
-		<td>". $row["Age"]. "</td>
-		<td>". $row["Blood_Type"]. "</td>
-		<td>". $row["ID_Territory"]. "</td>
-		<td>". $row["Date_Of_Murder"]. "</td>
-		</tr>";
-    }
-	echo "</table>";
-}
-else {
-    echo "No Vivtim Found";
-}
-$conn-> close();
-?>
-
-</div>
-</table>
+	<form method="POST" action="phpfun.php">
+		<input type="submit" name="Victims" value="Show Victims"/>
+    </form>
 </body>
 </html>
--- phpfun.php
+++ phpfun.php
@@ -1,40 +1,61 @@
 <?php
-    function Show_Victim(){
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>ID_Victim</th>";
-        echo "<th>ID_GS</th>";
-        echo "<th>Age</th>";
-        echo "<th>Blood_Type</th>";
-        echo "<th>ID_Territory</th>";
-        echo "<th>Date_Of_Murder</th>";
-        echo "</tr>";
-    $conn = mysqli_connect("localhost", "root", "", "se_project");
-    // Check connection, if failed show error
-    if ($conn-> connect_error) {
-        die("Connection failed:". $conn-> connect_error);
-    }
-    $sql = "SELECT * from victim";
-    $result = $conn -> query($sql);
-    
-    if ($result -> num_rows > 0){
-    // Output data for each row
-        while ($row = $result -> fetch_assoc()){
-            echo "<tr>
-            <td>". $row["ID_Victim"]. "</td>
-            <td>". $row["ID_GS"]. "</td>
-            <td>". $row["Age"]. "</td>
-            <td>". $row["Blood_Type"]. "</td>
-            <td>". $row["ID_Territory"]. "</td>
-            <td>". $row["Date_Of_Murder"]. "</td>
-            </tr>";
-        }
-    echo "</table>";
+        echo "<!DOCTYPE html>";
+        echo "<html>";
+        echo "<head>";
+        echo "<title>Victims Table</title>";
+        echo "<link rel='stylesheet' href='css/style.css'>";
+        echo "</head>";
+        echo "<body>";
+
+        if(isset($_POST['Victims'])){
+            echo "<h2>Victims</h2>";
+            echo "<div class='table-wrapper'>";
+            echo "<table class='fl-table'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th>ID Victim</th>";
+            echo "<th>ID GS</th>";
+            echo "<th>Age</th>";
+            echo "<th>Blood Type</th>";
+            echo "<th>ID Territory</th>";
+            echo "<th>Date Of Murder</th>";
+            echo "</tr>";
+            echo "</thead>";
+            
+            $conn = mysqli_connect("localhost", "root", "", "se_project");
+            // Check connection, if failed show error
+            if ($conn-> connect_error) {
+                die("Connection failed:". $conn-> connect_error);
+            }
+            $sql = "SELECT * from victim";
+            $result = $conn -> query($sql);
+            
+            if ($result -> num_rows > 0){
+            // Output data for each row
+                while ($row = $result -> fetch_assoc()){
+                    echo "<tr>
+                    <td>". $row["ID_Victim"]. "</td>
+                    <td>". $row["ID_GS"]. "</td>
+                    <td>". $row["Age"]. "</td>
+                    <td>". $row["Blood_Type"]. "</td>
+                    <td>". $row["ID_Territory"]. "</td>
+                    <td>". $row["Date_Of_Murder"]. "</td>
+                    </tr>";
+                }
+                echo "</table>";
+            }
+            else {
+                echo "No Vivtim Found";
+            }
+            $conn-> close();
+            
+            
+            echo "</div>";
+            echo "</table>";
     }
     else {
-        echo "No Vivtim Found";
-    }$conn-> close();
-
-    echo "</table>";
-}
+    echo" dhur";
+    }
+    echo "</body>";
+    echo "</html>";
 ?>
