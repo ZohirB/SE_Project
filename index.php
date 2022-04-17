6d7aac17705711dde842e0048c144c01083c1a6b
ZohirB <zohirbochi.zb@gmail.com>
2022-04-17 21:21:24 +0300

    main

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 <head>
     <title>SE_Project</title>
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/button.css">
+
 </head>
 <body>
 	<form method="POST" action="phpfun.php">
-		<input type="submit" name="Victims" value="Show Victims"/>
+	<input type="submit" name="Victims" value="Show Victims" class="button-18">
     </form>
 </body>
 </html>
+
+
