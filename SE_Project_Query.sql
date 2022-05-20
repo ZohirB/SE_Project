6c602adc42e8c2b16a556f853c810769bd6418cf
Abdalrahman Shayah <59410846+abdalrahmansh@users.noreply.github.com>
2022-05-20 23:26:31 +0300

    Update SE_Project_Query.sql

--- SE_Project_Query.sql
+++ SE_Project_Query.sql
@@ -1,13 +1,10 @@
 --تريغر لمناطق الدفن والخطف
-/*DELIMITER $$
-CREATE TRIGGER `updateProductPrice`
-BEFORE UPDATE ON `territory`
-FOR EACH ROW
-BEGIN
-  IF NEW.territory.name  like OLD.territory_name
+DELIMITER $$
+CREATE TRIGGER bef_ins_ter BEFORE INSERT ON territory
+ FOR EACH ROW BEGIN
+  IF NEW.territory.name  like territory.territory_name and new.territory_type <> territory.territory_type
     THEN
       SET NEW.id_territory=100235 ;
   END IF ;
 END$$
-DELIMITER ;*/
-
+DELIMITER ;
