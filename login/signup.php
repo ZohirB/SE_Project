271a2bb23c117b33675e41a7cb2c26918232e03d
raoof405 <abdalraoofabedalaya@gmail.com>
2022-05-21 14:46:56 +0300

    main
    Signed-off-by: raoof405 <abdalraoofabedalaya@gmail.com>

---
+++ login/signup.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>انشاء حساب</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+     <form action="signup-check.php" method="post">
+     	<h2>انشاء حساب</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+          <?php if (isset($_GET['success'])) { ?>
+               <p class="success"><?php echo $_GET['success']; ?></p>
+          <?php } ?>
+
+          <label>الاسم</label>
+          <?php if (isset($_GET['name'])) { ?>
+               <input type="text" 
+                      name="name" 
+                      
+                      value="<?php echo $_GET['name']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="name" 
+                      ><br>
+          <?php }?>
+
+          <label>الاسم الشخصي</label>
+          <?php if (isset($_GET['uname'])) { ?>
+               <input type="text" 
+                      name="uname" 
+                     
+                      value="<?php echo $_GET['uname']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="uname" 
+                      ><br>
+          <?php }?>
+
+
+     	<label>كلمة السر</label>
+     	<input type="password" 
+                 name="password" 
+                ><br>
+
+          <label>تاكيد كلمة السر</label>
+          <input type="password" 
+                 name="re_password" 
+                ><br>
+
+     	<button type="submit">تسجيل الدخول</button>
+          <a href="index.php" class="ca">هل لديك حساب مسبقا؟</a>
+     </form>
+</body>
+</html>
